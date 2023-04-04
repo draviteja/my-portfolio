1cd47dce2c9e69202812ddc15185298ec943872b
ravitejadaggupati <raviteja5255@gmail.com>
2023-04-05 02:16:39 +0530

    Updates

---
+++ precache-manifest.31a4dfbdf35831929aea60aa782b859e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cccc12b8c493a2462734e0c405fecbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d92a2615f2c8c9ee63c",
+    "url": "/static/css/2.04cfbadd.chunk.css"
+  },
+  {
+    "revision": "08ac0c48bd3eee04f7d1",
+    "url": "/static/css/main.66d03444.chunk.css"
+  },
+  {
+    "revision": "6d92a2615f2c8c9ee63c",
+    "url": "/static/js/2.a558b141.chunk.js"
+  },
+  {
+    "revision": "4167ad910959c003e49f1cb4e5a48b14",
+    "url": "/static/js/2.a558b141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ac0c48bd3eee04f7d1",
+    "url": "/static/js/main.22f02b5f.chunk.js"
+  },
+  {
+    "revision": "7c5e4ace7e9c5a53bd8e",
+    "url": "/static/js/runtime-main.06e93911.js"
+  },
+  {
+    "revision": "fee05782a7098aacdc469270b91152ac",
+    "url": "/static/media/avatar-1.fee05782.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
